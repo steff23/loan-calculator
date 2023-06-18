cffaee55af0d207c4775ae5dd46746953ebee8a5
Stefan Mitrovic <stefanmitrovic@Stefans-MacBook-Pro.local>
2023-06-18 18:58:00 +0200

    Initial commit

---
+++ src/test/java/com/leanpay/loancalculator/LoanCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leanpay.loancalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
